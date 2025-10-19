8c5c6500f402eae8a4d0651d1cf55957d1455006
mrl00 <m2l_0@outlook.com>
2025-10-19 14:17:50 -0300

    feat: implement kafka middleware

--- internal/kafka/kafka.go
+++ internal/kafka/kafka.go
@@ -4,21 +4,27 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log/slog"
+	"strconv"
 	"time"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type Config struct {
-	Brokers []string
-	Topic   string
-	GroupID string
+	Brokers           []string
+	Topic             string
+	GroupID           string
+	NumOfPartitions   int
+	ReplicationFactor int
 }
 
 func (c Config) GetBrokers() string {
-	var brokers = ""
-	for _, broker := range c.Brokers {
-		brokers = brokers + broker
+	var brokers = c.Brokers[0]
+	if len(c.Brokers) > 1 {
+		for _, broker := range c.Brokers[1:] {
+			brokers = brokers + "," + broker
+		}
 	}
 	return brokers
 }
@@ -32,9 +38,9 @@ type OrderEvent struct {
 
 func NewProducer(cfg Config) (*kafka.Producer, error) {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"boostrap.servers": cfg.GetBrokers(),
-		"acks":             "all",
-		"retries":          5,
+		"bootstrap.servers": cfg.GetBrokers(),
+		"acks":              "all",
+		"retries":           5,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create producer: %v", err)
@@ -73,5 +79,47 @@ func ProduceOrder(ctx context.Context, producer *kafka.Producer, order OrderEven
 		return ctx.Err()
 	}
 
+	return nil
+}
+
+func EnsureTopic(ctx context.Context, cfg Config) error {
+	admin, err := kafka.NewAdminClient(&kafka.ConfigMap{
+		"bootstrap.servers": cfg.GetBrokers(),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create admin client: %v", err)
+	}
+	defer admin.Close()
+
+	metadata, err := admin.GetMetadata(&cfg.Topic, false, 5000)
+	if err == nil && len(metadata.Topics) > 0 && len(metadata.Topics[cfg.Topic].Partitions) > 0 {
+		slog.Log(ctx, slog.LevelDebug, "topic %s already exists", cfg.Topic, nil)
+		return nil
+	}
+
+	topicSpec := []kafka.TopicSpecification{
+		{
+			Topic:             cfg.Topic,
+			NumPartitions:     cfg.NumOfPartitions,
+			ReplicationFactor: cfg.ReplicationFactor,
+		},
+	}
+	maxDur, err := time.ParseDuration("60s")
+	if err != nil {
+		panic("ParseDuration(60s)")
+	}
+	results, err := admin.CreateTopics(ctx, topicSpec, kafka.SetAdminOperationTimeout(maxDur))
+	if err != nil {
+		slog.Log(ctx, slog.LevelError, "failed to create topic %s: %v", cfg.Topic, err)
+		return err
+	}
+
+	for i, result := range results {
+		if result.Error.Code() != kafka.ErrNoError && result.Error.Code() != kafka.ErrTopicAlreadyExists {
+			slog.WarnContext(ctx, "%s: %s %v", strconv.Itoa(i), result.Topic, result.Error)
+		}
+	}
+
+	slog.Debug("")
 	return nil
 }
